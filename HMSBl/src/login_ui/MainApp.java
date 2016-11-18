9043909847bbe4139dde2f19bca0b6ca15ed5596
ObserverZQ <657282348@qq.com>
2016-11-18 18:30:12 +0800

    login_ui updated (try try)

---
+++ HMSBl/src/login_ui/MainApp.java
@@ -0,0 +1,16 @@
+package login_ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
