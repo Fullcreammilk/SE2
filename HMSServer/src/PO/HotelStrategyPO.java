d42faf3ca27bfdb802d60464ed102260c1b7988b
Fullcreammilk <852805936@qq.com>
2016-11-19 11:29:51 +0800

    po

--- HMSServer/src/PO/HotelStrategyPO.java
+++ HMSServer/src/PO/HotelStrategyPO.java
@@ -1,5 +1,36 @@
 package PO;
-
+enum HotelStrategyType{BIRTHDAY,COMPANY,BEYOND,DATE}
 public class HotelStrategyPO {
-
+	private String hotelname,strategyname,specialInfo;
+	double discount;
+	HotelStrategyType type;
+	
+	public HotelStrategyPO(String hotelname,String strategyname,String specialInfo,double discount,
+			HotelStrategyType t){
+		this.hotelname=hotelname;
+		this.strategyname=strategyname;
+		this.specialInfo=specialInfo;
+		this.discount=discount;
+		this.type=t;
+	}
+	
+	public String getHotelName(){
+		return this.hotelname;
+	}
+	
+	public String getStrategyName(){
+		return this.strategyname;
+	}
+	
+	public String getSpecialInof(){
+		return this.specialInfo;
+	}
+	
+	public double getDiscount(){
+		return this.discount;
+	}
+	
+	public HotelStrategyType getType(){
+		return this.type;
+	}
 }
